47568ecae1bb0d3a1f568c9020e6eed1c9a8dc7f
Barnabas001 <117881010+Barnabas001@users.noreply.github.com>
2026-01-30 18:08:10 +0100

    wrap App with provider

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { SavedJobsProvider } from "./context/SavedJobsProvider";
+import { ApplicationProvider } from "./context/ApplicationsProvider";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <SavedJobsProvider>
-      <App />
+      <ApplicationProvider>
+        <App />
+      </ApplicationProvider>
     </SavedJobsProvider>
   </React.StrictMode>,
 );
